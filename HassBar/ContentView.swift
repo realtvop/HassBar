4cae639110b126f406b6d82ab11bb691ae8fa3b2
realtvop <realtvop@realtvop.top>
2026-06-28 00:44:42 +0800

    initial commit

---
+++ HassBar/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HassBar
+//
+//  Created by realtvop on 2026/6/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HassBar/HassBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  HassBarApp.swift
+//  HassBar
+//
+//  Created by realtvop on 2026/6/28.
+//
+
+import SwiftUI
+
+@main
+struct HassBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
